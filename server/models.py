0c8f1afd9c4b5de8713f8663becc7b7cdd77c790
akamnovelakwa-ship-it <akam.akwa@student.moringaschool.com>
2026-01-16 07:04:43 +0300

    lab

--- server/models.py
+++ server/models.py
@@ -1,16 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class Message(db.Model, SerializerMixin):
-    __tablename__ = 'messages'
-
+class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String)
-    username = db.Column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    title = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=True)
 
-    def __repr__(self):
-        return f'<Message by {self.username}: {self.body[:10]}...>'
+    def to_dict(self):
+        return {"id": self.id, "title": self.title, "year": self.year}
--- server/seed.py
+++ server/seed.py
@@ -1,34 +1,9 @@
-#!/usr/bin/env python3
+from app import app, db
+from models import Movie
 
-from random import choice as rc
-
-from faker import Faker
-
-from app import app
-from models import db, Message
-
-fake = Faker()
-
-usernames = [fake.first_name() for i in range(4)]
-if "Duane" not in usernames:
-    usernames.append("Duane")
-
-def make_messages():
-
-    Message.query.delete()
-    
-    messages = []
-
-    for i in range(20):
-        message = Message(
-            body=fake.sentence(),
-            username=rc(usernames),
-        )
-        messages.append(message)
-
-    db.session.add_all(messages)
-    db.session.commit()        
-
-if __name__ == '__main__':
-    with app.app_context():
-        make_messages()
+with app.app_context():
+    db.create_all()
+    m1 = Movie(title="The Matrix", year=1999)
+    m2 = Movie(title="Inception", year=2010)
+    db.session.add_all([m1, m2])
+    db.session.commit()
